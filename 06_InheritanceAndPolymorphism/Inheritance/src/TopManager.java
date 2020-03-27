99b6cda557961043a9edb3409882f31b7637db3c
SharaevOleg <61539375+SharaevOleg@users.noreply.github.com>
2020-03-27 15:24:23 +0300

    6_4_inheritance_fixTheError

--- 06_InheritanceAndPolymorphism/Inheritance/src/TopManager.java
+++ 06_InheritanceAndPolymorphism/Inheritance/src/TopManager.java
@@ -1,10 +1,10 @@
-class TopManager implements Employee {
+class TopManager extends Company implements Employee {
 
     @Override
     public int getMonthSalary() {
         int monthSalary = 100_000;
-        Company company = new Company();
-        int income = company.income;
+//        Company company = new Company();
+//        int income = company.income;
         if (income > 10_000_000) {
             monthSalary = monthSalary + 150_000;
         }
