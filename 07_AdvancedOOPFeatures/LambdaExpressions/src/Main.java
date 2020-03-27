e2a68891e06d980556fac5b1dff221e9eef31514
SharaevOleg <61539375+SharaevOleg@users.noreply.github.com>
2020-03-28 02:47:58 +0300

    7_1_lambdaExpressions_done

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -7,14 +7,28 @@ import java.util.stream.Stream;
 
 public class Main
 {
-    private static String staffFile = "data/staff.txt";
+    private static String staffFile =
+            "E:\\Skillbox_JAVA-разработчик с нуля\\skillbox\\java_basics\\07_AdvancedOOPFeatures\\LambdaExpressions\\data\\staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
     public static void main(String[] args)
     {
         ArrayList<Employee> staff = loadStaffFromFile();
+//        Collections.sort(staff, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee o1, Employee o2) {
+//                return o1.getName().compareTo(o2.getName());
+//            }
+//        });
+        Collections.sort(staff, (o1, o2) -> {
+            if (0 == o1.getSalary().compareTo(o2.getSalary())){
+                return o1.getName().compareTo(o2.getName());
+            }
+            return o1.getSalary().compareTo(o2.getSalary());
+        });
 
-
+        staff.forEach(System.out :: println);
+//        for (Employee employee : staff){System.out.println(employee);}
     }
 
     private static ArrayList<Employee> loadStaffFromFile()
