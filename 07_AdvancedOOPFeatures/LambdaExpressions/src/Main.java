15f91a773a58e4a59de97692c0f599b384fd71f4
SharaevOleg <61539375+SharaevOleg@users.noreply.github.com>
2020-03-31 05:48:27 +0300

    7_2_lambdaExpressions_done

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -1,57 +1,53 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Stream;
 
-public class Main
-{
+public class Main {
     private static String staffFile =
             "E:\\Skillbox_JAVA-разработчик с нуля\\skillbox\\java_basics\\07_AdvancedOOPFeatures\\LambdaExpressions\\data\\staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         ArrayList<Employee> staff = loadStaffFromFile();
-//        Collections.sort(staff, new Comparator<Employee>() {
-//            @Override
-//            public int compare(Employee o1, Employee o2) {
-//                return o1.getName().compareTo(o2.getName());
-//            }
-//        });
-        Collections.sort(staff, (o1, o2) -> {
-            if (0 == o1.getSalary().compareTo(o2.getSalary())){
-                return o1.getName().compareTo(o2.getName());
-            }
-            return o1.getSalary().compareTo(o2.getSalary());
-        });
 
-        staff.forEach(System.out :: println);
-//        for (Employee employee : staff){System.out.println(employee);}
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//        Date date = sdf.parse("11-11-2018");
+//        staff.stream().filter(employee -> employee.getWorkStart().toString().substring(24, 28).equals("2017"))
+//                .forEach(System.out::println);
+        staff.stream().filter(employee -> employee.getWorkStart().toString().substring(24, 28).equals("2017"))
+                .max(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
+
+        Stream stream = staff.stream();
+        stream.filter(x -> x.equals(x.toString())).forEach(System.out::println);
+
+//        staff.forEach(System.out::println);
     }
 
-    private static ArrayList<Employee> loadStaffFromFile()
-    {
+    private static ArrayList<Employee> loadStaffFromFile() {
         ArrayList<Employee> staff = new ArrayList<>();
-        try
-        {
+
+        Stream stream = staff.stream();
+        stream.sorted(Comparator.comparing(Employee::getWorkStart)).forEach(System.out::println);
+
+        try {
             List<String> lines = Files.readAllLines(Paths.get(staffFile));
-            for(String line : lines)
-            {
+            for (String line : lines) {
                 String[] fragments = line.split("\t");
-                if(fragments.length != 3) {
+                if (fragments.length != 3) {
                     System.out.println("Wrong line: " + line);
                     continue;
                 }
                 staff.add(new Employee(
-                    fragments[0],
-                    Integer.parseInt(fragments[1]),
-                    (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
                 ));
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return staff;
