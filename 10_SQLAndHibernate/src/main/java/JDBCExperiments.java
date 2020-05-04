c33c1046bf57ae8c6a5dba9534de65b17599a934
SharaevOleg <61539375+SharaevOleg@users.noreply.github.com>
2020-05-05 00:54:03 +0300

    10_4_SQLAndHibernate_JDBCExperimentsChange

--- 10_SQLAndHibernate/src/main/java/JDBCExperiments.java
+++ 10_SQLAndHibernate/src/main/java/JDBCExperiments.java
@@ -9,31 +9,24 @@ public class JDBCExperiments {
         String urlNew = "jdbc:mysql://localhost:3306/skillbox?useLegacyDatetimeCode=false&serverTimezone=Australia/Sydney&useSSL=false";
         String user = "root";
         String pass = "mmm333";
-        int i = 01;
 
+        try {
+            Connection connection = DriverManager.getConnection(urlNew, user, pass);
+            Statement statment = connection.createStatement();
+            ResultSet resultSet = statment.executeQuery("SELECT *, COUNT(course_name)/12\n" +
+                    "FROM skillbox.purchaselist\n" +
+                    "GROUP BY course_name;");
 
-        for (; i <= 12; ) {
-            try {
-                Connection connection = DriverManager.getConnection(urlNew, user, pass);
-                Statement statment = connection.createStatement();
-                String str = String.valueOf(i);
-                ResultSet resultSet = statment.executeQuery("SELECT course_name, subscription_date, COUNT(course_name)\n" +
-                        "FROM skillbox.purchaselist\n" +
-                        "WHERE subscription_date BETWEEN STR_TO_DATE('2018-" + str + "-00 00:00:00', '%Y-%m-%d %H:%i:%s') \n" +
-                        "  AND STR_TO_DATE('2018-" + str + "-31 23:59:59', '%Y-%m-%d %H:%i:%s')\n" +
-                        "GROUP BY course_name;");
+            while (resultSet.next()) {
+                System.out.printf("%-35s --> %-2s%n", resultSet.getString("course_name"),
+                        resultSet.getString("COUNT(course_name)/12"));
 
-                System.out.println("\n****** " + "Среднее количество покупок " + str + " месяц");
-                while (resultSet.next()) {
-                    System.out.printf("%-35s --> %-2s%n", resultSet.getString("course_name"), resultSet.getString("COUNT(course_name)"));
-
-                }
-
-                i++;
-            } catch (Exception e) {
-                e.printStackTrace();
             }
 
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
+
